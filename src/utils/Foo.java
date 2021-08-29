0d77de7e5f4204d18de79503e63fbd53bba5a0ea
Noah Crumpton <nmcrumpton1@gmail.com>
2021-08-29 17:03:13 -0400

    heyNow method implemented

---
+++ src/utils/Foo.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("howdy");
+		System.out.println("hi from GitHub");
+		System.out.println("hi 'noah1' branch, from Eclipse");
+		System.out.println("hello worlds");
+		System.out.println("added line");
+	}
+	
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+		
+	}
+
+}
